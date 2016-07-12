b3173fcb8ef690b4bd336694a44a2cf4747f1212
notsoux <wellstaff@gmail.com>
2016-07-12 14:09:12 +0200

    first tests

--- test/testInsertQuote.js
+++ test/testInsertQuote.js
@@ -1,31 +1,83 @@
-var should = require('should');
+var assert = require('chai');
+var should = require('chai');
 var assert = require('assert');
 var request = require('supertest');
+require('should-http');
+
+const constant = require('../constant/Constant');
 
 process.env.MONGODB_CONNECTION_STRING = 'mongodb://localhost:27017/test_quotes';
 
-describe('Routing', function() {
+const dao = require('../dao/dao');
 
-    // within before() you can run all the operations that are needed to setup your tests. In this case
+describe('Add Quote', function() {
+
+    // within before() you can run all the operations that are needed to setup your test. In this case
     // I want to create a connection with the database, and when I'm done, I call done().
-    before(function (done) {
-        done();
+    beforeEach(function (done) {
+        dao.connect( function () {
+            dao.deleteAll( done);
+        });
     });
 
-    describe('Account', function() {
-        it('should return no quote on empty store', function (done) {
-            request( 'http://localhost:3000')
-                .get('/quote')
+
+    describe('Add one quote', function() {
+        let QUOTE_VAL = 'Test quote 1';
+        let AUTHOR_VAL = 'Test author 1';
+        it('should return inserted quote', function (done) {
+            request( 'http://localhost:3001')
+                .post('/quote/insert')
+                .send( {
+                    quote: QUOTE_VAL,
+                    author: AUTHOR_VAL
+                })
                 // end handles the response
                 .end(function(err, res) {
                     if (err) {
                         throw err;
                     }
-                    // this is should.js syntax, very clear
-                    res.should.have.status(400);
+                    //console.log(`res -> ${JSON.stringify( res)}`);
+                    res.should.have.status(200);
+                    //assert.notEqual( -1, res.text.indexOf( constant.QUOTE_NOT_AVAILABLE_MESSAGE));
                     done();
                 });
-        })
+        });
     });
+
+
+    describe('Add one quote and get it back', function() {
+        let QUOTE_VAL = 'Test quote 1';
+        let AUTHOR_VAL = 'Test author 1';
+        it('should return inserted quote', function (done) {
+            request( 'http://localhost:3001')
+                .post('/quote/insert')
+                .send( {
+                    quote: QUOTE_VAL,
+                    author: AUTHOR_VAL
+                })
+                // end handles the response
+                .end(function(err, res) {
+                    if (err) {
+                        throw err;
+                    }
+                    request( 'http://localhost:3001')
+                        .get('/quote/from_db')
+                        // end handles the response
+                        .end(function(err, res) {
+                            if (err) {
+                                throw err;
+                            }
+                            res.should.have.status(200);
+
+                            //check if quote contains quote / author inserted
+                            assert.notEqual( -1, res.text.indexOf( QUOTE_VAL));
+                            assert.notEqual( -1, res.text.indexOf( AUTHOR_VAL));
+                            done();
+                        });
+                });
+        });
+    });
+
+
 
 });
--- test/testNoQuote.js
+++ test/testNoQuote.js
@@ -10,7 +10,7 @@ process.env.MONGODB_CONNECTION_STRING = 'mongodb://localhost:27017/test_quotes';
 
 const dao = require('../dao/dao');
 
-describe('Quote', function() {
+describe('No Quote', function() {
 
     // within before() you can run all the operations that are needed to setup your test. In this case
     // I want to create a connection with the database, and when I'm done, I call done().
@@ -29,33 +29,9 @@ describe('Quote', function() {
                     if (err) {
                         throw err;
                     }
-                    console.log(`res.text -> ${res.text}`);
+                    //console.log(`res.text -> ${res.text}`);
                     res.should.have.status(200);
                     assert.notEqual( -1, res.text.indexOf( constant.QUOTE_NOT_AVAILABLE_MESSAGE));
-                    done();
-                });
-        })
-    });
-
-
-    describe('Add quote', function() {
-        let QUOTE_VAL = 'Test quote 1';
-        let AUTHOR_VAL = 'Test author 1';
-        it('should return inserted quote', function (done) {
-            request( 'http://localhost:3001')
-                .post('/quote/insert')
-                .send( {
-                    quote: QUOTE_VAL,
-                    author: AUTHOR_VAL
-                })
-                // end handles the response
-                .end(function(err, res) {
-                    if (err) {
-                        throw err;
-                    }
-                    console.log(`res -> ${JSON.stringify( res)}`);
-                    res.should.have.status(200);
-                    //assert.notEqual( -1, res.text.indexOf( constant.QUOTE_NOT_AVAILABLE_MESSAGE));
                     done();
                 });
         })
